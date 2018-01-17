cb4fd3c79c5772a6d4b5623a68f47f65d0147c63
Sirajus Salekin <ssalek14@earlham.edu>
2018-01-16 23:37:59 -0500

    refactoring the flask app

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# ride-share
-## About
-ride-share is a tool aimed to streamline finding rides within Earlham College community members. It uses Flask and PostgreSQL. However, it's still in beta mode. 
+# EC RideShare
+This is a web app intended for the Earlham College community members
+to share rides.
+
+## Tools
+We are using Flask, which is a lightweight web framework, ideal for our
+purposes. The backend is powered by PostgreSQL (as of now), but we plan
+on moving to a lighter, more convenient database.
+
+
+## Setup
+1. Clone this repo:
+```
+git clone https://github.com/salekinsirajus/ride-share.git
+```   
+2. Install the dependencies:
+    * wtforms 
+    * psycopg2
+    * flask 
+    * flask-wtforms
+    You can use pip to install all these:
+    ```
+    pip -r install requirements.txt
+    ```
+3. Make sure PostgreSQL is installed and ready to be used
+2. create a database named ecrideshare_db on postgresql 
+(check line 20 of flaskapp.py if there is a problem with db connection)
+3. Run create.sql file to create the tables.
+4. Now you should be able to run the app by typing "python flaskapp.py"
+5. Go to a browser and type localhost:5000/ and that should take you to the front page.
+
+## Comments
+1. We are assumming you enter valid values in all the fields in the form 
+    while posting for a ride.
+2. Right now, both links on the index page points to the same form
--- flaskapp.py
+++ flaskapp.py
@@ -34,59 +34,58 @@ app.config.from_object('config')
 app.secret_key = 'you_cant_know'
 
 class UserInput(Form):
-        fName = StringField(u'First Name', [validators.required("Enter your First Name")])
-        lName = StringField(u'Last Name', [validators.required("Enter your Last Name")])
-        origin = TextAreaField(u'Origin', [validators.required("Where are you leaving from?")])
-        destination = TextAreaField(u'Destination', [validators.required("And where are you going to?")])
-        dateEarly = DateField(u'Earliest Leaving',[validators.required("Enter the earliest\
-							date you are leaving.")])
-        dateLate = DateField(u'Latest Leaving',[validators.required("Enter the latest date\
-						you'll be leaving")])
-        money = FloatField(u'Gas Money', [validators.optional("How much do you want from your riders?")])
-        seats = IntegerField(u'Number of seats', [validators.optional("Is there enough room?")])
-        submit = SubmitField(u'Post')
+    fName = StringField(u'First Name', [validators.required("Enter your First Name")])
+    lName = StringField(u'Last Name', [validators.required("Enter your Last Name")])
+    origin = TextAreaField(u'Origin', [validators.required("Where are you leaving from?")])
+    destination = TextAreaField(u'Destination', [validators.required("And where are you going to?")])
+    dateEarly = DateField(u'Earliest Leaving',[validators.required("Enter the earliest\
+                                                                    date you are leaving.")])
+    dateLate = DateField(u'Latest Leaving',[validators.required("Enter the latest date\
+                                                                    you'll be leaving")])
+    money = FloatField(u'Gas Money', [validators.optional("How much do you want from your riders?")])
+    seats = IntegerField(u'Number of seats', [validators.optional("Is there enough room?")])
+    submit = SubmitField(u'Post')
 
 @app.route('/', methods = ['POST','GET'])
 def index():
-	return render_template("index.html")	
+    return render_template("index.html")
 
 @app.route('/dataInput', methods = ['GET','POST'])
 def dataInput():
-	form = UserInput(request.form)
+    form = UserInput(request.form)
 
-	if request.method == 'POST' and form.validate():
-		name = request.form[u'fName'] +" " +  request.form['lName']
-                origin = request.form[u'origin']
-                destination = request.form[u'destination']
-                dateearly = request.form[u'dateEarly']
-                datelate = request.form[u'dateLate']
-                money = request.form[u'money']
-                seats = request.form[u'seats']
+    if request.method == 'POST' and form.validate():
+        name = request.form[u'fName'] +" " +  request.form['lName']
+        origin = request.form[u'origin']
+        destination = request.form[u'destination']
+        dateearly = request.form[u'dateEarly']
+        datelate = request.form[u'dateLate']
+        money = request.form[u'money']
+        seats = request.form[u'seats']
+        session['name'] = name
+        sessionSA = Session()
 
-               	session['name'] = name
+        matchedTrip = []
+        print (matchedTrip)
+        for trip in sessionSA.query(trips):
+            if trip.origin==origin and trip.destination==destination:
+                matchedTrip.append(trip)
+        if len(matchedTrip) > 0:
+            print ("showing the list")
+            return render_template("landing.html", matchedTrip = matchedTrip, name = name)
+        newUser = users(name=name)
+        newTrip = trips(origin=origin, destination=destination, dateearly=dateearly, datelate=datelate, price=money)
+        sessionSA.add(newUser)
+        sessionSA.add(newTrip)
+        sessionSA.commit()
 
-		sessionSA = Session()
-		matchedTrip = []
-		print (matchedTrip)		
-                for trip in sessionSA.query(trips):
-			if trip.origin==origin and trip.destination==destination:
-				matchedTrip.append(trip)
-		if len(matchedTrip) > 0:
-			print ("showing the list")
-			return render_template("landing.html", matchedTrip = matchedTrip, name = name) 					
-		newUser = users(name=name)
-		newTrip = trips(origin=origin, destination=destination, dateearly=dateearly, datelate=datelate, price=money)
-		sessionSA.add(newUser)
-		sessionSA.add(newTrip)
-		sessionSA.commit()
-
-                return redirect(url_for('landing'))
-        return render_template('dataInput.html', form = form)
+        return redirect(url_for('landing'))
+    return render_template('dataInput.html', form = form)
 
 @app.route('/landing')
 def landing():
-	return render_template('landing.html', name=session['name'])
+    return render_template('landing.html', name=session['name'])
 
 if __name__ == '__main__':
-	app.debug= True
-	app.run()
+    app.debug= True
+    app.run()
